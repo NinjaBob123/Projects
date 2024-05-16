6ba5519722fa62a2632b2283d30549c94c4f344d
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-05-16 11:48:52 -0500

    Update breaker.py

--- break/breaker.py
+++ break/breaker.py
@@ -1,8 +1,23 @@
+from pynput.keyboard import Listener, Key
+breaker = False
+
+
+def end(key):
+  global breaker
+  if key == Key.esc:
+    breaker = True
+
+
 file = open("txt1.txt", "a")
+keyLstn = Listener(onpress=end)
 word = "autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism"
 while 1:
+  if breaker:
+    break
   try:
     while 1:
+      if breaker:
+        break
       file.write(word)
       word = word + " " + word
   except IOError:
