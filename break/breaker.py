8c3e43a4144dacf3b666f884b5ac3999c55e47ff
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-05-16 15:53:11 -0500

    Update breaker.py

--- break/breaker.py
+++ break/breaker.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from time import sleep
 breaker = False
+num = 1
 
 
 def end():
@@ -9,7 +10,7 @@ def end():
   breaker = True
 
 
-file = open("txt1.txt", "a")
+file = open(f"txt{1}.txt", "a")
 ender = Thread(target=end, args=None, daemon=True)
 ender.start()
 word = "autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism"
@@ -17,11 +18,13 @@ while 1:
   try:
     while 1:
       if breaker:
+        file.close()
         exit(0)
       file.write(word)
       word = word + " " + word
   except IOError:
     file.close()
-    file = open("txt2.txt", "x")
+    num += 1
+    file = open(f"txt{num}.txt", "x")
     file.close()
-    file = open("txt2.txt", "a")
+    file = open(f"txt{num}.txt", "a")
