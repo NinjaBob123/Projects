d3f9b1c4177f5684a4f591c6c49ef83ff25f67cc
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-05-17 11:42:37 -0500

    Update breaker.py

--- break/breaker.py
+++ break/breaker.py
@@ -1,27 +1,12 @@
-from threading import Thread
 from time import sleep
 breaker = False
 num = 1
-
-
-def end():
-  global breaker
-  sleep(20)
-  breaker = True
-
-
 file = open(f"txt{1}.txt", "a")
-ender = Thread(target=end, args=None, daemon=True)
-ender.start()
 word = "autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism"
-while 1:
+for x in range(100)
   try:
-    while 1:
-      if breaker:
-        file.close()
-        exit(0)
-      file.write(word)
-      word = word + " " + word
+    file.write(word)
+    word = word + " " + word
   except IOError:
     file.close()
     num += 1
