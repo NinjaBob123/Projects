05a8e56c765c834a6a14d44bee04faeff3ae63c9
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-05-17 19:01:10 -0500

    Update breaker.py

--- break/breaker.py
+++ break/breaker.py
@@ -1,15 +1,10 @@
 from time import sleep
 breaker = False
 num = 1
-file = open(f"txt{1}.txt", "a")
 word = "autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism autism"
 for x in range(100):
   try:
-    file.write(word)
+    print(word)
     word = word + " " + word
   except IOError:
-    file.close()
-    num += 1
-    file = open(f"txt{num}.txt", "x")
-    file.close()
-    file = open(f"txt{num}.txt", "a")
+    raise Error("File out of space, go to next file")
