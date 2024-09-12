9779cc5e4df68b7161ab98dd1f83691ad5244c86
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-12 18:59:05 -0500

    Update Neurons.py

--- AIProject1/Neurons.py
+++ AIProject1/Neurons.py
@@ -1,24 +1,75 @@
-from random import random
-class NeuralNet:
-  def __init__(self, inSize, hideSize, hideWidth, outSize):
-    self.initialize(inSize, hideSize, hideWidth, outSize)
-  def initialize(inParam, hideParam, width, outParam):
-    networkNeurons = []
-    for layer in range(width):
-      networkNeurons.append([])
-    for neronNum in range(inParam):
-      network[0].append({'input': None,
-                         'bias': None,
-                         'type': 'input'})
-    for layer in range(width):
-      for neronNum in range(hideParam):
-        network[layer + 1].append({'bias': None,
-                                   'type': 'hidden'})
-        network[layer + 1[neronNum]['bias'] = random(0.0, 10.0)
+import random
 
-    for neronNum in range(outParam):
-      network[2].append({'bias': None,
-                         'output': None,
-                         'type': 'output'})
-    print(networkNeurons)
-    networkConnections = []
+class NeuralNet:
+    def __init__(self, inSize, hideSize, hideWidth, outSize):
+        self.initialize(inSize, hideSize, hideWidth, outSize)
+
+    def initialize(self, inParam, hideParam, width, outParam):
+        # Initialize network neurons
+        networkNeurons = [[], []]
+        for _ in range(width):
+            networkNeurons.append([])
+
+        print("Neurons before population:", networkNeurons)
+
+        # Add input neurons
+        for neuronNum in range(inParam):
+            networkNeurons[0].append({
+                'input': None,
+                'bias': None,
+                'type': 'input'
+            })
+
+        # Add hidden neurons
+        for layer in range(width):
+            for neuronNum in range(hideParam):
+                neuron = {
+                    'bias': None,
+                    'type': 'hidden'
+                }
+                neuron['bias'] = random.random() + random.randint(0, 3)
+                networkNeurons[layer + 1].append(neuron)
+
+        # Add output neurons
+        for neuronNum in range(outParam):
+            networkNeurons[-1].append({
+                'bias': None,
+                'output': None,
+                'type': 'output'
+            })
+
+        print("Neurons after population:", networkNeurons)
+
+        # Initialize network connections
+        networkConnections = [[], []]
+        for _ in range(width):
+            networkConnections.append([])
+
+        # Create connections from input to hidden neurons
+        for connection in range(inParam):
+            for neuron in range(len(networkNeurons[1])):
+                networkConnections[0].append({
+                    'from': f'i{connection}',
+                    'to': f'h{neuron}',
+                    'val': random.random() + random.randint(0, 3)
+                })
+
+        # Create connections from hidden neurons to output neurons
+        for layer in range(width):
+            if width > 1:
+                for neuron in range(len(networkNeurons[layer + 1])):
+                    if layer == width - 1:
+                        for outNeuronNum in range(len(networkNeurons[-1])):
+                            networkConnections[layer + 1].append({
+                                'from': f'h{neuron}',
+                                'to': f'o{outNeuronNum}',
+                                'val': random.random() + random.randint(0, 3)
+                            })
+            else:
+              pass
+              # will add hidden to hidden after chat fix
+
+        print("Network connections:", networkConnections)
+
+# Example usage
+net = NeuralNet(4, 1, 4, 1)
