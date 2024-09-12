ba976ff30edb32df32424fecd100b7196d01f3ae
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-12 14:21:25 -0500

    Create Neurons.py

---
+++ AIProject1/Neurons.py
@@ -0,0 +1,23 @@
+from random import random
+class NeuralNet:
+  def __init__(self, inSize, hideSize, outSize):
+    self.initialize(inSize, hideSize, outSize)
+  def initialize(inParam, hideParam, outParam):
+    network = []
+    for neronNum in range(inParam):
+      network[0].append({'input': None,
+                         'weight': None,
+                         'bias': None,
+                         'type': 'input'})
+    for neronNum in range(hideParam):
+      network[1].append({'weight': None,
+                         'bias': None,
+                         'type': 'hidden'})
+      network[1][neronNum]['weight'] = random(0.0, 10.0)
+      network[1][neronNum]['bias'] = random(0.0, 10.0)
+
+    for neronNum in range(outParam):
+      network[2].append({'weight': None,
+                         'bias': None,
+                         'output': None,
+                         'type': 'output'})
