d19b9dddca4a68169ce04366b786ad30432b412e
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2024-09-23 20:08:31 +0000

    Update mapper.py

--- MicromouseCode/mapper.py
+++ MicromouseCode/mapper.py
@@ -1,4 +1,4 @@
-import imageio as imgio #add imageio
+import imageio as imgio #add imageio # type: ignore
 import numpy as npy
 values = npy.ndarray('32x32x3')
 img = imgio.imwrite('map.png', values)
