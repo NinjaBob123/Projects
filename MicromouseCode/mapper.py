23e743092594f7ab680fbc5c3e39193c5dbd9e6e
NinjaBob123 <153332837+NinjaBob123@users.noreply.github.com>
2024-09-24 20:28:06 -0500

    Update and run mapper.py

--- MicromouseCode/mapper.py
+++ MicromouseCode/mapper.py
@@ -1,4 +1,11 @@
 import imageio as imgio #add imageio
 import numpy as npy
-values = npy.ndarray((32, 32, 3))
+values = npy.ndarray((32, 32, 3), dtype=npy.uint8)
+print(values)
+for x in range(len(values)):
+    for y in range(len(values[x])):
+        values[x][y][0] = 0
+        values[x][y][1] = 0
+        values[x][y][2] = 0
+print(values)
 img = imgio.imwrite('map.png', values)
